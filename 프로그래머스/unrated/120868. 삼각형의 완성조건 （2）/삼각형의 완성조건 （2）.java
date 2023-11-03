b07a07ef7cd809b5d80e5270c65dd87233302395
JP-company <77595494+JP-company@users.noreply.github.com>
2023-11-03 10:51:25 +0900

    [level unrated] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/unrated/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int[] sides) {
         int smallInt = sides[0] > sides[1] ? sides[1] : sides[0];
         
-        return 2 * smallInt - 1 ;
+        return 2*smallInt-1;
     }
 }
