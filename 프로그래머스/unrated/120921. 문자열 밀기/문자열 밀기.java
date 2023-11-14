1179c8aa5e74751423850836b857a021b693dbbf
JP-company <77595494+JP-company@users.noreply.github.com>
2023-11-14 19:24:20 +0900

    [level unrated] Title: 문자열 밀기, Time: 1.78 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        
         for (int i = 0; i < A.length(); i++) {
             String str = A.substring(A.length() - i) + A.substring(0, A.length() - i);
             if (str.equals(B)) {
                 return i;
             }
         }
-        
         return -1;
     }
 }
