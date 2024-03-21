b495e88623e0210f3ec6ce62e94f77f442d0bd95
JP-company <77595494+JP-company@users.noreply.github.com>
2024-03-21 13:53:37 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2664 ms, Memory: 324172 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        
+        int[] arr = new int[size];
+        for (int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        mergeSort(arr, 0, size-1);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<size; i++) {
+            sb.append(arr[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+    
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            
+            merge(arr, left, mid, right);
+        }
+    }
+    
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int i = left;
+        int j = mid+1;
+        
+        int size = right - left + 1;
+        int[] tempArr = new int[size];
+        int k = 0;
+        
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                tempArr[k++] = arr[i++];
+                continue;
+            }
+            tempArr[k++] = arr[j++];
+        }
+        
+        if (i > mid) {
+            while (j <= right) {
+                tempArr[k++] = arr[j++];
+            }
+        } else {
+            while (i <= mid) {
+                tempArr[k++] = arr[i++];
+            }
+        }
+
+        for (int l=0; l<size; l++) {
+            arr[left + l] = tempArr[l];
+        }
+    }
+}
